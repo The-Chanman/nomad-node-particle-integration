609531f65ffae6c95c2343a5604c6c3d067a2c81
Reid Williams <rwilliams@ideo.com>
2016-11-17 12:56:54 -0800

    initial commit, node works

---
+++ sensor.js
@@ -0,0 +1,59 @@
+const Nomad = require('nomad-stream')
+const moment = require('moment')
+const nomad = new Nomad()
+const fetch = require('node-fetch')
+
+let instance = null
+
+// parse into url object 
+let base = 'http://erddap.exploratorium.edu:8080/erddap/tabledap/exploreusgsdata.json?time,temperature,specific_conductance,salinity,turbidity,dissolved_o2,station_id,latitude,longitude&time>='
+
+function buildUrl() {
+  const d = new moment()
+  const hourAgo = d.subtract(6, 'h')
+  const timeString = hourAgo.toISOString()
+  const url = `${base}${timeString}`
+  const encoded = encodeURI(url)
+  return encoded
+}
+
+function transform(json) {
+  let transformed = {}
+  transformed['columnNames'] = json.table.columnNames
+  transformed['columnTypes'] = json.table.columnTypes
+  transformed['columnUnits'] = json.table.columnUnits
+  transformed['data'] = json.table.rows[0]
+  return transformed
+}
+
+// returns promise
+function getMessage() {
+  const url = buildUrl()
+  return fetch(url).then(res => {
+    return res.json();
+  }).then(json => {
+    const formatted = transform(json)
+    return Promise.resolve(JSON.stringify(formatted))
+  }).catch(err => {
+    console.log(err)
+    return Promise.reject(err)
+  })
+}
+
+nomad.prepareToPublish().then((n) => {
+  instance = n
+  return instance.publishRoot('hello')
+}).then(() => {
+  setInterval(() => {
+    getMessage().then(m => {
+      instance.publish(m)
+      .catch(err => {
+        console.log(`Error: ${err}`)
+      })
+    })
+    .catch(err => {
+      console.log(`Error: ${err}`)
+    })
+  }, 60000)  
+})
+
