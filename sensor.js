cb1e8978a8551e9c8dea109abdb088a874d766dc
The-Chanman <ecphylo@gmail.com>
2016-11-29 00:51:41 -0800

    added correct URL and fixed time to be 1 hour

--- sensor.js
+++ sensor.js
@@ -7,11 +7,12 @@ let instance = null
 const frequency = 60 * 60 * 1000
 
 // parse into url object 
-let base = 'http://erddap.exploratorium.edu:8080/erddap/tabledap/exploreusgsdata.json?time,temperature,specific_conductance,salinity,turbidity,dissolved_o2,station_id,latitude,longitude&time>='
+let base = 'http://erddap.exploratorium.edu:8080/erddap/tabledap/explorebeaconbay5min.htmlTable?time%2CTemperature_1%2CAir_Pressure%2CTemperature_2%2CRelative_Humidity%2CDew_Point%2CO3_1%2CCO_1%2CNO2_1%2CO3_Working_2%2CO3_Auxiliary_2%2CCO_Working_2%2CCO_Auxiliary_2%2CNO_Working%2CNO_Auxiliary%2CNO_Auxiliary_2%2CNO2_Working_2%2CNO2_Auxiliary_2%2CParticulate_high%2CParticulate_total%2CParticulate_pct%2CCO2%2CTemperature_3%2Cstation_id%2Clatitude%2Clongitudetime>='
+//'http://erddap.exploratorium.edu:8080/erddap/tabledap/exploreusgsdata.json?time,temperature,specific_conductance,salinity,turbidity,dissolved_o2,station_id,latitude,longitude&time>='
 
 function buildUrl() {
   const d = new moment()
-  const hourAgo = d.subtract(6, 'h')
+  const hourAgo = d.subtract(1, 'h')
   const timeString = hourAgo.toISOString()
   const url = `${base}${timeString}`
   const encoded = encodeURI(url)
