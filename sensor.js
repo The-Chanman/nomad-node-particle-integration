4c942dcf5b7ba21094da47745161baed34dd1fbf
The-Chanman <ecphylo@gmail.com>
2016-11-30 14:46:17 -0800

    works with gas and UV no units yet

--- sensor.js
+++ sensor.js
@@ -1,14 +1,23 @@
 const Nomad = require('nomad-stream')
-const moment = require('moment')
 const nomad = new Nomad()
+const moment = require('moment')
 const fetch = require('node-fetch')
+const Particle = require('particle-api-js')
+var particle = new Particle()
+const credentials = require('./particle-login')
+
+
+//Particle Device Setup
+//IDEO Müich
+const deviceID = '370034000f47343432313031'
+const events = ['Gas', 'UV']
+
+// Pub data
+var pubdata = {Gas: "", Gas_Time: "", UV: "", UV_Time: ""}
 
 let instance = null
-const frequency = 60 * 60 * 1000
-
-// parse into url object 
-let base = 'http://erddap.exploratorium.edu:8080/erddap/tabledap/explorebeaconbay5min.json?time,Temperature_1,Air_Pressure,Temperature_2,Relative_Humidity,Dew_Point,O3_1,CO_1,NO2_1,O3_Working_2,O3_Auxiliary_2,CO_Working_2,CO_Auxiliary_2,NO_Working,NO_Auxiliary,NO_Auxiliary_2,NO2_Working_2,NO2_Auxiliary_2,Particulate_high,Particulate_total,Particulate_pct,CO2,Temperature_3,station_id,latitude,longitude&time>='
-//'http://erddap.exploratorium.edu:8080/erddap/tabledap/exploreusgsdata.json?time,temperature,specific_conductance,salinity,turbidity,dissolved_o2,station_id,latitude,longitude&time>='
+var lastPub = 0
+const timeBetween = 10000
 
 function buildUrl() {
   const d = new moment()
@@ -19,45 +28,96 @@ function buildUrl() {
   return encoded
 }
 
-function transform(json) {
-  let transformed = {}
-  transformed['columnNames'] = json.table.columnNames
-  transformed['columnTypes'] = json.table.columnTypes
-  transformed['columnUnits'] = json.table.columnUnits
-  transformed['data'] = json.table.rows[0]
-  return transformed
-}
 
-// returns promise
-function getMessage() {
-  const url = buildUrl()
-  return fetch(url).then(res => {
-  	console.log(res.body)
-    return res.json();
-  }).then(json => {
-    const formatted = transform(json)
-    return Promise.resolve(JSON.stringify(formatted))
-  }).catch(err => {
-    console.log(err)
-    return Promise.reject(err)
-  })
-}
+particle.login(credentials).then(res => {
+  token = res.body.access_token
+  console.log(`Got Token: ${token}`)
 
-nomad.prepareToPublish().then((n) => {
+  return nomad.prepareToPublish()
+}).then((n) => {
   instance = n
   return instance.publishRoot('hello')
 }).then(() => {
-  setInterval(() => {
-    getMessage().then(m => {
-      instance.publish(m)
+
+  //declaring last publish data
+  lastPub = new moment()
+  return particle.getEventStream({ deviceId: deviceID, auth: token })
+}).then(s => {
+  stream = s
+  stream.on('event', data => {
+    // console.log(data)
+    
+    switch(data.name) {
+    case 'Gas':
+        pubdata.Gas = data.data
+        pubdata.Gas_Time = data.published_at
+        console.log(data)
+        break;
+    case 'UV':
+        pubdata.UV = data.data
+        pubdata.UV_Time = data.published_at
+        console.log(data)
+        break;
+    default:
+        console.log(data.name + " is value of " + data.data + " but not accounted for right now.")
+    }
+    // this determines frequency of transmission 
+    var currentTime = new moment()
+    if (currentTime - lastPub >= timeBetween){
+      if (pubdata.Gas === "" || pubdata.Gas_Time === "" || pubdata.UV === "" || pubdata.UV_Time === ""){
+      // do nothing
+      } else {
+      // publish if everything is full
+      console.log("**********************************************************************************************")
+      console.log(pubdata)
+      console.log("**********************************************************************************************")
+
+      instance.publish(JSON.stringify(pubdata))
       .catch(err => {
-        console.log(`Error: ${err}`)
+        console.log(`Error: ${JSON.stringify(err)}`)
       })
-    })
-    .catch(err => {
-      console.log(`Error: ${err}`)
-    })
-  }, frequency)  
+
+      pubdata = {Gas: "", Gas_Time: "", UV: "", UV_Time: ""}
+      lastPub = currentTime
+      }
+    }
+  })
+}).catch(err => {
+    console.log(`Error: ${JSON.stringify(err)}`)
 })
+
+
+
+// // returns promise
+// function getMessage() {
+//   const url = buildUrl()
+//   return fetch(url).then(res => {
+//   	console.log(res.body)
+//     return res.json();
+//   }).then(json => {
+//     const formatted = transform(json)
+//     return Promise.resolve(JSON.stringify(formatted))
+//   }).catch(err => {
+//     console.log(err)
+//     return Promise.reject(err)
+//   })
+// }
+
+// nomad.prepareToPublish().then((n) => {
+//   instance = n
+//   return instance.publishRoot('hello')
+// }).then(() => {
+//   setInterval(() => {
+//     getMessage().then(m => {
+//       instance.publish(m)
+//       .catch(err => {
+//         console.log(`Error: ${err}`)
+//       })
+//     })
+//     .catch(err => {
+//       console.log(`Error: ${err}`)
+//     })
+//   }, frequency)  
+// })
 
 
