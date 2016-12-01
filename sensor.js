b09d8f755ff9e824e0acb1d3e0f2bdabf9ae8246
The-Chanman <ecphylo@gmail.com>
2016-11-30 20:55:36 -0800

    challenge to abstract from gavin

--- sensor.js
+++ sensor.js
@@ -1,90 +1,114 @@
 const Nomad = require('nomad-stream')
-const nomad = new Nomad()
 const moment = require('moment')
 const fetch = require('node-fetch')
 const Particle = require('particle-api-js')
-var particle = new Particle()
+
 const credentials = require('./particle-login')
 
+const particle = new Particle()
+const nomad = new Nomad()
 
 //Particle Device Setup
-//IDEO Müich
+//IDEO Münich
 const deviceID = '370034000f47343432313031'
-const events = ['Gas', 'UV']
 
 // Pub data
-var pubdata = {Gas: "", Gas_Time: "", UV: "", UV_Time: ""}
+// let pubdata = {Gas: "", Gas_Time: "", UV: "", UV_Time: ""}
 
-let instance = null
-var lastPub = 0
-const timeBetween = 10000
+// [Gas,UV,Time]
 
-function buildUrl() {
-  const d = new moment()
-  const hourAgo = d.subtract(2, 'h')
-  const timeString = hourAgo.toISOString()
-  const url = `${base}${timeString}`
-  const encoded = encodeURI(url)
-  return encoded
+class MessageToPublish {
+  constructor(arrayOfEventNames){
+    this.data = {}
+    for (let value of arrayOfEventNames){
+      this.data[value]
+    }
+  }
+
+
 }
 
 
+let currentPublishData = null
+const defaultPublishData = { gas: "", gasTime: "", uv: "", uvTime: "" }
+
+
+
+function setTime() {
+  return new moment()
+}
+
+
+let instance = null
+let lastPub = null
+const timeBetween = 10 * 1000 //10 seconds
+const timeThreshold = 30 * 60 * 1000 // 30 minutes
+
+
 particle.login(credentials).then(res => {
-  token = res.body.access_token
-  console.log(`Got Token: ${token}`)
-
-  return nomad.prepareToPublish()
-}).then((n) => {
-  instance = n
-  return instance.publishRoot('hello')
-}).then(() => {
-
-  //declaring last publish data
-  lastPub = new moment()
-  return particle.getEventStream({ deviceId: deviceID, auth: token })
-}).then(s => {
-  stream = s
-  stream.on('event', data => {
-    // console.log(data)
-    
-    switch(data.name) {
-    case 'Gas':
-        pubdata.Gas = data.data
-        pubdata.Gas_Time = data.published_at
-        console.log(data)
-        break;
-    case 'UV':
-        pubdata.UV = data.data
-        pubdata.UV_Time = data.published_at
-        console.log(data)
-        break;
-    default:
-        console.log(data.name + " is value of " + data.data + " but not accounted for right now.")
-    }
-    // this determines frequency of transmission 
-    var currentTime = new moment()
-    if (currentTime - lastPub >= timeBetween){
-      if (pubdata.Gas === "" || pubdata.Gas_Time === "" || pubdata.UV === "" || pubdata.UV_Time === ""){
-      // do nothing
-      } else {
-      // publish if everything is full
-      console.log("**********************************************************************************************")
-      console.log(pubdata)
-      console.log("**********************************************************************************************")
-
-      instance.publish(JSON.stringify(pubdata))
-      .catch(err => {
-        console.log(`Error: ${JSON.stringify(err)}`)
-      })
-
-      pubdata = {Gas: "", Gas_Time: "", UV: "", UV_Time: ""}
-      lastPub = currentTime
-      }
-    }
+    let token = res.body.access_token
+    console.log(`Got Token: ${token}`)
+    return nomad.prepareToPublish()
   })
-}).catch(err => {
-    console.log(`Error: ${JSON.stringify(err)}`)
-})
+  .then((n) => {
+    instance = n
+    return instance.publishRoot('hello')
+  })
+  .then(() => {
+    //declaring last publish date
+    lastPub = setTime()
+    return particle.getEventStream({ deviceId: deviceID, auth: token })
+  })
+  .then(s => {
+    stream = s
+    stream.on('event', data => {
+      // console.log(data)
+      console.log(data)
+
+
+
+      // switch(data.name) {
+      //   case 'Gas':
+      //     pubdata.Gas = data.data
+      //     pubdata.Gas_Time = data.published_at
+      //     console.log(data)
+      //     break;
+      //   case 'UV':
+      //     pubdata.UV = data.data
+      //     pubdata.UV_Time = data.published_at
+      //     console.log(data)
+      //     break;
+      //   default:
+      //     console.log(data.name + " is value of " + data.data + " but not accounted for right now.")
+      }
+      // this determines frequency of transmission 
+      let currentTime = setTime()
+      let timeSince = currentTime - lastPub
+      if (timeSince >= timeBetween){
+        if (pubdata.Gas === "" || pubdata.Gas_Time === "" || pubdata.UV === "" || pubdata.UV_Time === ""){
+          
+        } else {
+        // publish if everything is full
+        console.log("***************************************************************************************")
+        console.log(pubdata)
+        console.log("***************************************************************************************")
+
+        instance.publish(JSON.stringify(pubdata))
+          .catch(err => console.log(`Error: ${JSON.stringify(err)}`))
+        pubdata = {Gas: "", Gas_Time: "", UV: "", UV_Time: ""}
+        lastPub = currentTime
+        }
+      }
+      // if haven't receieved anything in the time frame
+      if (timeSince >= timeThreshold){
+        //publish what we got
+        instance.publish(JSON.stringify(pubdata))
+          .catch(err => console.log(`Error: ${JSON.stringify(err)}`))
+      }
+
+    })
+  })
+  .catch(err => console.log(`Error: ${JSON.stringify(err)}`))
 
 
 
