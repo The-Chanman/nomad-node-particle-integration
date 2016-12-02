76b630eddcbf24b2250089bd4f75c3db2dc8216f
The-Chanman <ecphylo@gmail.com>
2016-12-01 20:38:41 -0800

    cleaned up and updated package.json

--- sensor.js
+++ sensor.js
@@ -12,128 +12,127 @@ const nomad = new Nomad()
 //IDEO Münich
 const deviceID = '370034000f47343432313031'
 
-let currentPublishData = null
 let instance = null
 let lastPub = null
+let token
 
-const defaultPublishData = { Gas: "", GasUnits: "parts per million", GasTime: "", UV: "", UVUnits: "nm", UVTime: "" }
+const defaultPublishData = { 
+  gas: {
+    data: "",
+    units: "parts per million",
+    time: "",
+  },
+  uv: {
+    data: "",
+    units: "nm",
+    time: "",
+  },
+  humidity: {
+    data: "",
+    units: "%",
+    time: "",
+  },
+  temperature: {
+    data: "",
+    units: "celsius",
+    time: "",
+  }
+}
 const timeBetween = 10 * 1000 //10 seconds
 const timeThreshold = 30 * 60 * 1000 // 30 minutes
 
 class DataMaintainer {
   constructor(){
-    currentPublishData = defaultPublishData
+    this.data = defaultPublishData
   }
   setValue(key, value){
-    if(currentPublishData === undefined){
-      console.log('Recieved invalid key:', key)
-      break
+    let cleanedKey = this.cleanKey(key)
+    if(cleanedKey in this.data){
+      this.data[cleanedKey].data = value.data
+      this.data[cleanedKey].time = value.time
+    } else {
+      this.data[cleanedKey] = value
     }
-    currentPublishData[key] = value
+  }
+  cleanKey(key){
+    let cleanedKey = key.replace(/\s+/, '\x01').split('\x01')[0]
+    cleanedKey = cleanedKey.toLowerCase()
+    return cleanedKey
   }
   getAll(){
-    return currentPublishData
+    return this.data
   }
   isAllFilled(){
-    return currentPublishData["gas"] && currentPublishData["gasTime"] && currentPublishData["uv"] && currentPublishData[uvTime]
+    return this.data["gas"]["data"] && this.data["gas"]["time"] && this.data["uv"]["data"] && this.data["uv"]["time"]
   }
   clear(){
-    currentPublishData = defaultPublishData
+    this.data = defaultPublishData
   }
   toString(){
-    return JSON.stringify(currentPublishData)
+    return JSON.stringify(this.data)
   }
 }
 
-function setTime() {
+function getTime() {
   return new moment()
 }
 
 //init data manager
 let dataManager = new DataMaintainer()
 
-particle.login(credentials).then(res => {
-    let token = res.body.access_token
+particle.login(credentials)
+  .then(res => {
+    token = res.body.access_token
     console.log(`Got Token: ${token}`)
     return nomad.prepareToPublish()
   })
   .then((n) => {
     instance = n
-    return instance.publishRoot('hello')
+    return instance.publishRoot('hello this is IDEO Münich')
   })
   .then(() => {
     //declaring last publish date
-    lastPub = setTime()
+    lastPub = getTime()
     return particle.getEventStream({ deviceId: deviceID, auth: token })
   })
   .then(s => {
     stream = s
     stream.on('event', data => {
-      // console.log(data)
       console.log(data)
-      dataManager.setValue(data.name, data.data)
-      dataManager.setValue(data.name + "Time", data.published_at)
+      try{dataManager.setValue(data.name, {data: data.data, time: data.published_at})}
+      catch(err){
+        console.log("DataMaintainer failed with error of " + err)
+      }
       // this determines frequency of transmission 
-      let currentTime = setTime()
+      let currentTime = getTime()
       let timeSince = currentTime - lastPub
       if (timeSince >= timeBetween){
-        if (dataManager.isAllFilled){
-        // publish if everything is full
-        console.log("***************************************************************************************")
-        console.log(dataManager.getAll())
-        console.log("***************************************************************************************")
 
-        instance.publish(dataManager.toString())
-          .catch(err => console.log(`Error: ${JSON.stringify(err)}`))
-        dataManager.clear()  
-        lastPub = currentTime
+        console.log("timeSince >= timeBetween")
+
+        if (dataManager.isAllFilled){
+          // publish if everything is full
+          console.log("***************************************************************************************")
+          console.log(dataManager.getAll())
+          console.log("***************************************************************************************")
+
+          instance.publish(dataManager.toString())
+            .catch(err => console.log(`Error: ${JSON.stringify(err)}`))
+          dataManager.clear()  
+          lastPub = currentTime
         }
       }
       // if haven't receieved anything in the time frame
       if (timeSince >= timeThreshold){
-        //publish what we got
+        // publish what we got
         instance.publish(dataManager.toString())
           .catch(err => console.log(`Error: ${JSON.stringify(err)}`))
+        console.log("***************************************************************************************")
+        console.log(dataManager.getAll())
+        console.log("***************************************************************************************")
         dataManager.clear()  
         lastPub = currentTime
       }
-
     })
   })
   .catch(err => console.log(`Error: ${JSON.stringify(err)}`))
-
-
-
-// // returns promise
-// function getMessage() {
-//   const url = buildUrl()
-//   return fetch(url).then(res => {
-//   	console.log(res.body)
-//     return res.json();
-//   }).then(json => {
-//     const formatted = transform(json)
-//     return Promise.resolve(JSON.stringify(formatted))
-//   }).catch(err => {
-//     console.log(err)
-//     return Promise.reject(err)
-//   })
-// }
-
-// nomad.prepareToPublish().then((n) => {
-//   instance = n
-//   return instance.publishRoot('hello')
-// }).then(() => {
-//   setInterval(() => {
-//     getMessage().then(m => {
-//       instance.publish(m)
-//       .catch(err => {
-//         console.log(`Error: ${err}`)
-//       })
-//     })
-//     .catch(err => {
-//       console.log(`Error: ${err}`)
-//     })
-//   }, frequency)  
-// })
-
-
